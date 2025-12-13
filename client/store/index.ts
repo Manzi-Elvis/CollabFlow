23139fe593ef16d1e90dad7830613e264b672498
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-14 00:03:43 +0200

    ...

---
+++ client/store/index.ts
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './auth/authSlice'
+import workspaceReducer from './workspace/workspaceSlice'
+import boardsReducer from './boards/boardsSlice'
+import uiReducer from './ui/uiSlice'
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    workspace: workspaceReducer,
+    boards: boardsReducer,
+    ui: uiReducer,
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
