3ea5b6f4656c087cfad83e5147e25595c45c3757
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-14 00:28:25 +0200

    ...

--- client/components/boards/BoardView.tsx
+++ client/components/boards/BoardView.tsx
@@ -1,9 +1,30 @@
 'use client'
 
+import { useSelector } from 'react-redux'
+import { RootState } from '@/store'
+import Column from './Column'
+import EmptyBoard from './EmptyBoard'
+
 export default function BoardView() {
+  const { activeBoardId, boards, columns } = useSelector(
+    (state: RootState) => state.boards
+  )
+
+  if (!activeBoardId) return <EmptyBoard />
+
+  const board = boards[activeBoardId]
+  if (!board) return <EmptyBoard />
+
   return (
-    <div className="flex gap-4 p-4 overflow-x-auto">
-      {/* Columns go here */}
+    <div className="flex h-full gap-4 p-4 overflow-x-auto">
+      {board.columnIds.map(columnId => (
+        <Column key={columnId} column={columns[columnId]} />
+      ))}
+
+      {/* Add Column placeholder */}
+      <button className="min-w-[280px] h-fit rounded-xl border border-dashed p-4 text-sm text-muted-foreground hover:bg-muted">
+        + Add column
+      </button>
     </div>
   )
 }
