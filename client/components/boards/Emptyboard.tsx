2db6ab353ea07429e417621da7dbf402b57b69d2
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-14 00:32:40 +0200

    ...

---
+++ client/components/boards/Emptyboard.tsx
@@ -0,0 +1,7 @@
+export default function EmptyBoard() {
+  return (
+    <div className="flex h-full items-center justify-center text-muted-foreground">
+      Select or create a board to get started
+    </div>
+  )
+}
