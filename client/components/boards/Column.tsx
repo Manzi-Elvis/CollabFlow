ac57aab8eebce7ee3201f038d97b07326965f7b5
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-14 00:30:41 +0200

    ...

---
+++ client/components/boards/Column.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import ColumnHeader from './ColumnHeader'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/store'
+
+type ColumnProps = {
+  column: {
+    id: string
+    name: string
+    taskIds: string[]
+  }
+}
+
+export default function Column({ column }: ColumnProps) {
+  const tasks = useSelector((state: RootState) => state.boards.tasks)
+
+  return (
+    <div className="flex flex-col w-[280px] bg-muted rounded-xl p-3">
+      <ColumnHeader title={column.name} />
+
+      <div className="flex flex-col gap-2 mt-3">
+        {column.taskIds.map(taskId => (
+          <div
+            key={taskId}
+            className="rounded-lg bg-background p-3 text-sm shadow-sm"
+          >
+            {tasks[taskId]?.title}
+          </div>
+        ))}
+      </div>
+
+      <button className="mt-3 text-xs text-muted-foreground hover:text-foreground">
+        + Add task
+      </button>
+    </div>
+  )
+}
