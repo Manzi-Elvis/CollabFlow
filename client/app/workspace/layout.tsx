19520996d533edee692ecf860ea5bc6f1a379ab5
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-14 19:10:33 +0200

    ...

--- client/app/workspace/layout.tsx
+++ client/app/workspace/layout.tsx
@@ -1,18 +1,13 @@
-'use client'
-
-import { useSelector } from 'react-redux'
-import { RootState } from '@/store'
-import { redirect } from 'next/navigation'
+import ModalRoot from '@/components/common/ModalRoot'
 
 export default function WorkspaceLayout({ children }: { children: React.ReactNode }) {
-  const isAuthenticated = useSelector((s: RootState) => s.auth.isAuthenticated)
-
-  if (!isAuthenticated) redirect('/login')
-
   return (
     <div className="flex h-screen">
       <aside className="w-64 border-r">Sidebar</aside>
-      <main className="flex-1 overflow-hidden">{children}</main>
+      <main className="flex-1 overflow-hidden relative">
+        {children}
+        <ModalRoot />
+      </main>
     </div>
   )
 }
